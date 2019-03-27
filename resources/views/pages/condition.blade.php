4925291b96b9f5448021f621b7f414d4be968a3f
Arnab Das <arnab.das682@gmail.com>
2019-03-27 18:35:30 +0600

    if elseif else condition test

--- resources/views/pages/condition.blade.php
+++ resources/views/pages/condition.blade.php
@@ -1,7 +1,9 @@
 <h2>{{$name}}</h2>
-{{strlen($name)}}
+{{strlen($name)}} <hr>
 @if(strlen($name) < 0)
 	String
+@elseif(strlen($name) > 5 && strlen($name) < 14)
+	String {{strlen($name)}}
 @else
 	Str
 @endif
